b7a9ebbdca6d6e18147634d55f089ff3ff018a4f
juntiwx <juntima.wx@gmail.com>
2023-11-12 12:45:06 +0700

    add inertia with vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import '../css/app.css'
+
+import {createApp, h} from 'vue'
+import {createInertiaApp} from '@inertiajs/vue3'
+
+createInertiaApp({
+    // Vite
+    resolve: name => {
+        const pages = import.meta.glob('./pages/**/*.vue', {eager: true})
+        return pages[`./pages/${name}.vue`]
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .mount(el)
+    },
+}).then(r => console.log('complete'))
